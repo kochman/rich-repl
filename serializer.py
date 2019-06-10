607da2da06787f7b202f123e6b2f303f7187cec4
Sidney Kochman <sidney@kochman.org>
2019-06-10 18:01:06 -0400

    fix dict serialization

--- serializer.py
+++ serializer.py
@@ -1,3 +1,6 @@
+import uuid
+
+
 class Serializer:
     def __init__(self):
         pass
@@ -10,19 +13,24 @@ class Serializer:
             return self.serialize_dict(obj)
         return self.serialize_primitive(obj)
 
-    def serialize_dict(self, d, seen=[]):
-        s = {}
+    def serialize_dict(self, d, seen={}):
+        s = {"type": "dict", "values": [], "id": str(uuid.uuid4())}
+
+        # store this dictionary's ID first
+        seen[id(d)] = s["id"]
+
         for k, v in d.items():
             if isinstance(v, dict):
-                if v in seen:
-                    val = {"type": "reference", "value": seen.index(v)}
+                if id(v) in seen:
+                    val = {"type": "reference", "value": seen[id(v)], "key": k}
                 else:
-                    seen.append(v)
+                    seen[id(v)] = s["id"]
                     val = self.serialize_dict(v, seen)
-                s[k] = val
+                s["values"].append(val)
             else:
                 val = self.serialize(v)
-                s[k] = val
+                val["key"] = k
+                s["values"].append(val)
         return s
 
     def serialize_primitive(self, p):
